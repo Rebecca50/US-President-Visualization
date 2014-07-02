8d55887af33b4da8e4d615df6538ff4a1b75e637
Rebecca50 <rebeccastrong50@gmail.com>
2014-07-02 18:11:18 -0400

    Poti circle size corresponds with term in office

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,8 +1,8 @@
 
 function d3Project(data){
-// console.log(data);
+// console.log(data.days);
 var svg = d3.select('svg');
-
+  // debugger;
 var circles = svg.selectAll('circle')
   .data(data)
   .enter()
@@ -12,7 +12,7 @@ var circles = svg.selectAll('circle')
     .data(data)
     .transition()
       .duration(600)
-        .attr('r', function(d){ return '20px'})
+        .attr('r', function(d){ return (d.days/100)+'px'})
         .attr('cx', function(){ return (Math.random()*100) + '%'})
         .attr('cy', function(){ return (Math.random()*100) + '%'})
         .style('opacity', function(){ return (Math.random()*100)})
